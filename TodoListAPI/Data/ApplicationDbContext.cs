dc3a652d9124643b43c818970c1af8de33ecdc66
MICHAEL NWACHUKWU <NUMICHAEL.DEV@GMAIL.COM>
2022-11-01 14:56:08 +0100

    Add project files.

---
+++ TodoListAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TodoListAPI.Models.AppModels;
+using TodoListAPI.Models.Authentication;
+
+namespace TodoListAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ToDoItemModel>(entity =>
+            {
+                entity.Property(e => e.ItemName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+                entity.Property(e => e.ItemDescription)
+                .IsRequired()
+                .HasMaxLength(100);
+
+                entity.Property(e => e.ItemStatus)
+                .IsRequired()
+                .HasMaxLength(1);
+            });
+
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<ToDoItemModel> ToDoItems { get; set; }
+    }
+}
---
+++ TodoListAPI/Models/Authentication/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace TodoListAPI.Models.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
